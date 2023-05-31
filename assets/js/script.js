e554937a876a9a48dea5f198ec8e8892586a4160
Ali200119 <aliit@code.edu.az>
2023-06-01 02:39:47 +0400

    feat: Tab-Menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,30 @@ $(document).ready(function() {
         nextArrow: '<i class="fa-solid fa-chevron-right next"></i>'
     });
 });
+
+
+
+// Tab-Menu
+
+let headers = document.querySelectorAll("#games .tab-menu-header ul li");
+let items = document.querySelectorAll("#games .tab-menu-items .items");
+
+
+for (const header of headers) {
+    header.addEventListener("click", function() {
+        document.querySelector("#games .tab-menu-header ul .active").classList.remove("active");
+        this.classList.add("active");
+
+        for (const item of items) {
+            if (item.getAttribute("data-category") == this.getAttribute("data-category")) {
+                item.style.opacity = 1;
+                item.style.pointerEvents = "unset";
+            }
+
+            else {
+                item.style.opacity = 0;
+                item.style.pointerEvents = "none";
+            }
+        }
+    });
+}
