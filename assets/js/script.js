07ec11fc2569e1fa2698014fbac3afed8e27f24b
Ali200119 <aliit@code.edu.az>
2023-06-01 00:14:03 +0400

    feat: Blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-// Parallax
+// Fav-Game Parallax
 
 let image = document.querySelector("#fav-game img");
 
@@ -8,4 +8,18 @@ window.addEventListener("scroll", function() {
     let scrollPosition = window.pageYOffset;
 
     image.style.transform = `translateY(${scrollPosition * 0.4}px)`;
+});
+
+
+
+// Blog Slider
+
+$(document).ready(function() {
+    $("#blog .blog-items").slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<i class="fa-solid fa-chevron-left prev"></i>',
+        nextArrow: '<i class="fa-solid fa-chevron-right next"></i>'
+    });
 });
